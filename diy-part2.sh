12f7cbbb6ddefed91dced906fb5afa4d131a1960
joshuazai <58354020+joshuazai@users.noreply.github.com>
2026-08-17 15:01:53 +0800

    Refactor configuration settings in diy-part2.sh
    Updated script to configure Wi-Fi, timezone, and zram settings.

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,18 +12,24 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # 3. 设置主机名
 sed -i 's/OpenWrt/K2T-AP/g' package/base-files/files/bin/config_generate
 
-# 4. 设置默认时区为上海 (CST-8)
-sed -i "s/'UTC'/'CST-8'/g" package/base-files/files/bin/config_generate
-sed -i "/set system.@system\[-1\].timezone='CST-8'/a \\\t\tset system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
-
-# 5. 默认开启 Wi-Fi 并设置国家码为 CN
+# 4. 默认开启 Wi-Fi
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/country=US/country=CN/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# 6. 设置 32MB zram 压缩内存交换
+# 5. 通过 uci-defaults 统一配置时区、无线国家码及 32MB zram 内存压缩
 mkdir -p package/base-files/files/etc/uci-defaults
 cat << 'EOF' > package/base-files/files/etc/uci-defaults/99-custom-settings
+# 设置时区为上海 CST-8
+uci set system.@system[0].timezone='CST-8'
+uci set system.@system[0].zonename='Asia/Shanghai'
+
+# 设置 32MB zram 压缩内存交换（适配 64MB RAM）
 uci set system.@system[0].zram_size_mb='32'
+
+# 预设无线国家码为中国 (CN)
+uci set wireless.radio0.country='CN'
+uci set wireless.radio1.country='CN'
+
 uci commit system
+uci commit wireless
 exit 0
 EOF
