5ada55fc4bf1c633e08d9543b56cb364a98fbf89
joshuazai <58354020+joshuazai@users.noreply.github.com>
2026-08-17 13:58:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # diy-part2.sh
 
-# 1. 彻底解决 wpad 包冲突（从底层默认依赖直接替换为支持完整 kvr 漫游的 wpad-mbedtls）
-find target/linux/ -name "Makefile" -exec sed -i 's/wpad-basic-mbedtls/wpad-mbedtls/g' {} +
+# 1. 全局替换默认无线组件为完整版 wpad-mbedtls (彻底根治 package_install 冲突)
 sed -i 's/wpad-basic-mbedtls/wpad-mbedtls/g' include/target.mk
+sed -i 's/wpad-basic-mbedtls/wpad-mbedtls/g' target/linux/ath79/Makefile
+find target/linux/ -name "Makefile" -exec sed -i 's/wpad-basic-mbedtls/wpad-mbedtls/g' {} +
 
-# 2. 修改默认管理后台 IP（如需主路由模式可改回 192.168.1.1）
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+# 2. 修改默认管理后台 IP (192.168.2.1，避免与光猫/主路由 1.1 冲突)
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# 3. 修改主机名
+# 3. 设置主机名
 sed -i 's/OpenWrt/K2T-AP/g' package/base-files/files/bin/config_generate
 
-# 4. 修改默认时区为上海 (CST-8)
+# 4. 设置默认时区为上海 (CST-8)
 sed -i "s/'UTC'/'CST-8'/g" package/base-files/files/bin/config_generate
 sed -i "/set system.@system\[-1\].timezone='CST-8'/a \\\t\tset system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
 
@@ -19,7 +20,7 @@ sed -i "/set system.@system\[-1\].timezone='CST-8'/a \\\t\tset system.@system[-1
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/country=US/country=CN/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# 6. 预置 32MB zram 压缩内存交换
+# 6. 设置 32MB zram 压缩内存交换
 mkdir -p package/base-files/files/etc/uci-defaults
 cat << 'EOF' > package/base-files/files/etc/uci-defaults/99-custom-settings
 uci set system.@system[0].zram_size_mb='32'
